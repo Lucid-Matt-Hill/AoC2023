1d67d07c01ad83a34c637faeca52a8778811c2ef
Lucid-Matt-Hill <152613502+Lucid-Matt-Hill@users.noreply.github.com>
2023-12-06 07:52:36 +0000

    Day 6

---
+++ 6th-Part-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _6th_Part_1
+{
+    struct Race
+    {
+        public int Duration;
+        public int RecordDistance;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var races = LoadRaces("input.txt");
+            var winningButtonDurations = races.Select(CalculateWinningButtonDurations).ToArray();
+            var totalWaysToWin = winningButtonDurations.Select(x => x.Count());
+
+            var answer = totalWaysToWin.Aggregate(1, (x, y) => x * y);
+            Console.Write(answer);
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<int> CalculateWinningButtonDurations(Race race)
+        {
+            var times = Enumerable.Range(0, race.Duration);
+            return times.Where(x => CalculateDistanceForTimeHeld(x, race.Duration) > race.RecordDistance);
+        }
+
+        private static int CalculateDistanceForTimeHeld(int timeHeld, int raceDuration)
+        {
+            return (timeHeld * (raceDuration - timeHeld));
+        }
+
+        private static IEnumerable<int> ParseNumbers(string line)
+        {
+            return line.Split(":")[1].Split(" ").Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse);
+        }
+
+        private static IEnumerable<Race> LoadRaces(string filename)
+        {
+            var lines = File.ReadLines(filename).ToArray();
+            var times = ParseNumbers(lines[0]);
+            var distances = ParseNumbers(lines[1]);
+            return times.Zip(distances, (a, b) => new Race() { Duration = a, RecordDistance = b });
+        }
+    }
+}
---
+++ 6th-Part-2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _6th_Part_2
+{
+    struct Race
+    {
+        public long Duration;
+        public long RecordDistance;
+    }
+
+    class Program
+    {
+
+        static double SolveQuadraticPlus(double a, double b, double c)
+        {
+            return (-b + Math.Sqrt((b * b) - (4 * a * c))) / (2 * a);
+        }
+
+        static double SolveQuadraticMinus(double a, double b, double c)
+        {
+            return (-b - Math.Sqrt((b * b) - (4 * a * c))) / (2 * a);
+        }
+
+        static void Main(string[] args)
+        {
+            var race = LoadRace("input.txt");
+            var winningButtonDurations = CalculateWinningButtonDurations(race);
+
+            // Translation to quadratic equation
+            // x * (Duration - x) = Distance
+            // 0 = x * (Duration - x) -1Distance
+            // 0 = -1x^2 + DurationX -1Distance
+
+            var testa = SolveQuadraticPlus(-1, race.Duration, -race.RecordDistance);
+            var testb = SolveQuadraticMinus(-1, race.Duration, -race.RecordDistance);
+
+            var answer = Math.Ceiling(testb) - Math.Ceiling(testa);
+
+            Console.Write(answer);
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<long> CalculateWinningButtonDurations(Race race)
+        {
+            return Enumerable.Empty<long>();
+            //var times = Enumerable.Range(0, race.Duration);
+            //return times.Where(x => CalculateDistanceForTimeHeld(x, race.Duration) > race.RecordDistance);
+        }
+
+        private static long CalculateDistanceForTimeHeld(long timeHeld, long raceDuration)
+        {
+            return (timeHeld * (raceDuration - timeHeld));
+        }
+
+        private static long ParseNumber(string line)
+        {
+
+            var rawNumberString = line.Split(":")[1];
+            var cleanNumberString = rawNumberString.ToCharArray().Where(c => !Char.IsWhiteSpace(c)).ToArray();
+
+            return long.Parse(cleanNumberString);
+        }
+
+        private static Race LoadRace(string filename)
+        {
+            var lines = File.ReadLines(filename).ToArray();
+            return new Race()
+            {
+                Duration = ParseNumber(lines[0]),
+                RecordDistance = ParseNumber(lines[1]),
+            };
+        }
+    }
+}
